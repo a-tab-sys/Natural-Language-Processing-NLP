baec415fed068200e45722818d53458819eff056
a-tab-sys <ammaarah.t33@gmail.com>
2025-03-08 08:17:56 -0500

    fifth commit

--- myenv/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-for i in corpus:
-#     words=nltk.word_tokenize(i)
-#     for word in words:
-#         if word not in set(stopwords.words('english')):     #applying english stopwords
-#             # print(stemmer.stem(word))
-#             print(lemmatizer.lemmatize(word))
